bbc253c13a2793f2e7fc91f570c7a86448b33549
Angeline Ho <angelineho16.03@gmail.com>
2024-05-07 20:28:12 +0700

    Initialize master branch

--- routes/api.php
+++ routes/api.php
@@ -26,5 +26,5 @@ Route::get('/cities', [CityController::class, 'index']);
 Route::get('/subdistricts', [SubDistrictController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']); 
 });
